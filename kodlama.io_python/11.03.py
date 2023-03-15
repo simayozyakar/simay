badb5f596cdf2b6884627ccd2ad2275787cd769a
varenot7withu <127473851+varenot7withu@users.noreply.github.com>
2023-03-15 17:39:59 +0300

    Create 11.03.py

---
+++ kodlama.io_python/11.03.py
@@ -0,0 +1,10 @@
+SinifListesi=["İsim Soyisim"]
+EklenecekIsim=input("İsim ve soyisim yazınız: ")
+SinifListesi.append(EklenecekIsim)
+CikarilacakIsim=input("Çıkarılacak isim ve soyismi yazınız: ")
+SinifListesi.remove(CikarilacakIsim)
+
+
+
+
+
